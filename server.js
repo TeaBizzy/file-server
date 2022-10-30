4adcfee83c2fb3a50d98a5bee510d72f7c3700fb
Stefan <satalbot@protonmail.com>
2022-10-30 15:52:32 +0000

    Implement available file search when the home directory is queried

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const fs = require('fs');
 
 const server = express();
 const port = 3000;
 
 
 server.use(express.static('files'));
-server.get("/files/", (req, res) => {
-  res.send('Hai Werld!')
+
+server.get("/", (req, res) => {
+  getFilePaths((files) => {res.send(JSON.stringify(files))});
 });
+
+const getFilePaths = function(callback) {
+  fs.readdir('./files', (error, files) => {
+    console.log(files);
+    callback(files);
+  });
+};
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`); // Tell yourself the port number to prevent mistakes in the future.
